7be7ebd013c714c3dd8da680601023639c46802f
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 04:32:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 def prime? (num)
-  if num > 1 
+  if num > 1
     for i in 2..(num - 1)
       if (num % i) == 0
         return false
       else
         return true
       end
-    end 
-  else 
+    end
+  else
     return false
   end
 end
